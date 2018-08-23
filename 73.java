26860d6721ec340c48b4ffd6a6bc3e2e9dc3763d
narendrabeeram <41769627+narendrabeeram@users.noreply.github.com>
2018-08-23 09:52:32 +0530

    Create 73.java

---
+++ 73.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int l=s.nextInt();
+		int r=s.nextInt();
+		if(n>l && n<r)
+		{
+			System.out.println("yes");
+		}
+		else
+		System.out.println("no");
+	}
+}
